6c000a9cdb317048a08f84d4a8c89ed5d64b0e45
Vishnu Singh <singh.vishnu8369@gmail.com>
2024-07-14 19:10:20 +0530

    Add files via upload

---
+++ frontend/js/contact.js
@@ -0,0 +1,46 @@
+let send = document.getElementById('send');
+
+let firstname = document.getElementById('firstname');
+let lastname = document.getElementById('lastname');
+let mobile = document.getElementById('mobile');
+let email = document.getElementById('email');
+let state = document.getElementById('state');
+let pincode = document.getElementById('pincode');
+let message = document.getElementById('message');
+let notification = document.getElementById('notification');
+
+async function senddata(event) {
+    event.preventDefault();
+
+    let userdata = {
+        firstname: firstname.value,
+        lastname:  lastname.value,
+        mobile:    mobile.value,
+        email:     email.value,
+        state:     state.value,
+        pincode:   pincode.value,
+        message:   message.value,
+    }
+    const exp = await axios.post('https://13.233.10.96:3000/user/addContact', userdata );
+    console.log(exp.data.status);
+    if(exp.data.status === true) {
+        showstatus();
+        console.log('got hit');
+    } 
+}
+
+function showstatus() {
+    firstname.value='';
+    lastname.value ='';
+    mobile.value   ='';
+    email.value    ='';
+    state.value    ='';
+    pincode.value  ='';
+    message.value  ='';
+
+    notification.style.display = "flex";
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 5000);
+    
+}
---
+++ frontend/js/free-site-survey.js
@@ -0,0 +1,52 @@
+let send = document.getElementById('send');
+
+let name = document.getElementById('name');
+let companyname = document.getElementById('companyname');
+let mobilenumber = document.getElementById('mobilenumber');
+let emailid = document.getElementById('emailid');
+let address = document.getElementById('address');
+let pincode = document.getElementById('pincode');
+let chooseanindustryorapplication = document.getElementById('chooseanindustryorapplication');
+let lengthofcoolingarea = document.getElementById('lengthofcoolingarea');
+let widthofcoolingarea = document.getElementById('widthofcoolingarea');
+let message = document.getElementById('message');
+
+async function senddata(event) {
+    event.preventDefault();
+    let userdata = {
+        name: name.value,
+        companyname: companyname.value,
+        mobilenumber: mobilenumber.value,
+        emailid: emailid.value,
+        address: address.value,
+        pincode: pincode.value,
+        chooseanindustryorapplication: chooseanindustryorapplication.value,
+        lengthofcoolingarea: lengthofcoolingarea.value,
+        message: message.value,
+    }
+    console.log(userdata);
+    const exp = await axios.post('https://13.233.10.96:3000/user/addSurvey', userdata);
+    console.log(exp.data.status);
+    if (exp.data.status === true) {
+        showstatus();
+        console.log('got hit');
+    }
+}
+
+function showstatus() {
+    name.value = '';
+    companyname.value = '';
+    mobilenumber.value = '';
+    emailid.value = '';
+    address.value = '';
+    pincode.value = '';
+    chooseanindustryorapplication.value = '';
+    lengthofcoolingarea.value = '';
+    message.value  ='';
+
+    notification.style.display = "flex";
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 5000);
+}
+
